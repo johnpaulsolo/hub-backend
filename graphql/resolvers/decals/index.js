994aaa81bf263e4c03f1f964ff3236ba1ba6bf02
@johsol <johnpaulfsolo@gmail.com>
2021-05-12 11:18:10 +0800

    FEAT: Send Decals function

--- graphql/resolvers/decals/index.js
+++ graphql/resolvers/decals/index.js
@@ -2,6 +2,27 @@ const Decals = require('../../../models/decals/index');
 
 module.exports = {
     SendDecals: (args) => {
-        console.log(args)
+        const {
+            UserId,
+            Pic1,
+            Pic2,
+            Date
+        } = args.newDecal
+
+        const SetDecals = new Decals({
+            UserId: UserId,
+            Pic1: Pic1,
+            Pic2: Pic2,
+            Date: Date
+        })
+
+        return SetDecals.save()
+            .then( result => {
+                return {
+                    ...result._doc
+                }
+            }).catch(err => {
+                throw err;
+            })
     }
 }
